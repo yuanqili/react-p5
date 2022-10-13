e5ff2ff97aafd2e17469b16150c91f8a4fee48a8
yuanqili <yuanqili1018@gmail.com>
2022-10-14 03:35:19 +0800

    first react-p5js application

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,50 @@
-import './index.css'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>Hello, World</p>
-        <p>Project: react-p5js</p>
-      </header>
-    </div>
-  )
+import P5Wrapper from '../Processing'
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      slider: 100,
+      frameRate: null,
+    }
+  }
+
+  onSetAppState = (newState, cb) => this.setState(newState, cb)
+
+  onSliderChange = (event) => this.setState({ slider: +event.target.value })
+
+  render() {
+    return (
+      <>
+        <P5Wrapper
+          p5Props={{ slider: this.state.slider }}
+          onSetAppState={this.onSetAppState}
+        />
+
+        <div style={{ textAlign: 'center' }}>
+          <strong>{this.state.slider}</strong>
+          <br />
+          <input
+            type='range'
+            min={5}
+            max={290}
+            step={1}
+            value={this.state.slider}
+            style={{ width: '90%', maxWidth: '900px' }}
+            onChange={this.onSliderChange}
+          />
+        </div>
+
+        <p style={{ textAlign: 'center' }}>
+          Sketch frame rate:&nbsp;
+          <big>
+            <strong>{this.state.frameRate}</strong>
+          </big>
+          &nbsp;fps
+        </p>
+      </>
+    )
+  }
 }
-
-export default App
